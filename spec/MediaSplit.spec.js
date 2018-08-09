9f982096fd036a1203580c5734295fb76c74cff3
Thiago Oliveira <thiago_ogt@outlook.com>
2018-08-09 19:35:17 -0300

    Allow specific time ranges It is now possible to specify a specific time range for a section. Eg: [00:50 - 03:20.200] Intro

--- spec/MediaSplit.spec.js
+++ spec/MediaSplit.spec.js
@@ -7,8 +7,12 @@ describe('MediaSplit', function () {
   let split
 
   describe('_isUrl', function () {
-    afterEach(() => split = null)
-    beforeEach(() => split = new MediaSplit())
+    afterEach(() => {
+      split = null
+    })
+    beforeEach(() => {
+      split = new MediaSplit()
+    })
 
     it('should match url', () => {
       expect(split._isUrl('http://www.youtube.com/watch?v=-wtIMTCHWuI')).to.true
@@ -26,7 +30,9 @@ describe('MediaSplit', function () {
   })
 
   describe('_parseMedia', function () {
-    afterEach(() => split = null)
+    afterEach(() => {
+      split = null
+    })
 
     it('should parse sections correctly', () => {
       split = new MediaSplit({
@@ -35,39 +41,45 @@ describe('MediaSplit', function () {
           '[00:00] foo',
           '[01:30] bar',
           '[03:28.222] Test _ file',
-          '[05:52.1] Qux',
+          '[05:52.1 - 07:24] Qux - abc',
+          '[07:50] -[Song - tag][name]'
         ]
       })
 
       const parsed = split._parseMedia()
-      expect(parsed).to.length(4)
+      expect(parsed).to.length(5)
 
-      expect(parsed[0].trackName).to.be.equals('foo')
-      expect(parsed[0].name).to.be.equals('foo.m4a')
-      expect(parsed[0].start).to.be.equals('00:00')
-      expect(parsed[0].end).to.be.equals('01:30')
+      expect(parsed[ 0 ].trackName).to.be.equals('foo')
+      expect(parsed[ 0 ].name).to.be.equals('foo.m4a')
+      expect(parsed[ 0 ].start).to.be.equals('00:00')
+      expect(parsed[ 0 ].end).to.be.equals('01:30')
 
-      expect(parsed[1].trackName).to.be.equals('bar')
-      expect(parsed[1].name).to.be.equals('bar.m4a')
-      expect(parsed[1].start).to.be.equals('01:30')
-      expect(parsed[1].end).to.be.equals('03:28.222')
+      expect(parsed[ 1 ].trackName).to.be.equals('bar')
+      expect(parsed[ 1 ].name).to.be.equals('bar.m4a')
+      expect(parsed[ 1 ].start).to.be.equals('01:30')
+      expect(parsed[ 1 ].end).to.be.equals('03:28.222')
 
-      expect(parsed[2].trackName).to.be.equals('Test _ file')
-      expect(parsed[2].name).to.be.equals('Test _ file.m4a')
-      expect(parsed[2].start).to.be.equals('03:28.222')
-      expect(parsed[2].end).to.be.equals('05:52.1')
+      expect(parsed[ 2 ].trackName).to.be.equals('Test _ file')
+      expect(parsed[ 2 ].name).to.be.equals('Test _ file.m4a')
+      expect(parsed[ 2 ].start).to.be.equals('03:28.222')
+      expect(parsed[ 2 ].end).to.be.equals('05:52.1')
 
-      expect(parsed[3].trackName).to.be.equals('Qux')
-      expect(parsed[3].name).to.be.equals('Qux.m4a')
-      expect(parsed[3].start).to.be.equals('05:52.1')
-      expect(parsed[3].end).to.be.equals(null)
+      expect(parsed[ 3 ].trackName).to.be.equals('Qux - abc')
+      expect(parsed[ 3 ].name).to.be.equals('Qux - abc.m4a')
+      expect(parsed[ 3 ].start).to.be.equals('05:52.1')
+      expect(parsed[ 3 ].end).to.be.equals('07:24')
+
+      expect(parsed[ 4 ].trackName).to.be.equals('-[Song - tag][name]')
+      expect(parsed[ 4 ].name).to.be.equals('-[Song - tag][name].m4a')
+      expect(parsed[ 4 ].start).to.be.equals('07:50')
+      expect(parsed[ 4 ].end).to.be.equals(null)
     })
 
     it('should throw on invalid start', () => {
       split = new MediaSplit({
-        sections: ['[00:AB.!] FOO']
+        sections: [ '[00:AB.!] FOO' ]
       })
-      expect(() => split._parseMedia()).to.throw('Unable to extract start time from line 1')
+      expect(() => split._parseMedia()).to.throw('Line 1 does not contain a valid time range')
     })
 
     it('should throw on invalid end', () => {
@@ -77,8 +89,7 @@ describe('MediaSplit', function () {
           '[AB:CC] BAR'
         ]
       })
-      expect(() => split._parseMedia()).to.throw('Unable to extract end time from line 2')
+      expect(() => split._parseMedia()).to.throw('Line 2 does not contain a valid time range')
     })
   })
 })
-

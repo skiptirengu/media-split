92a57c765a046f4928bcc64ea2728561718e67ef
Thiago Oliveira <thiago_ogt@outlook.com>
2018-08-14 16:16:57 -0300

    Refactoring cli code - Extracted new command module to make it testable

---
+++ cli/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const yargs = require('yargs')
+const Command = require('./Command.js')
+
+const argv = yargs
+  .usage('Usage: $0 <command> [options]')
+  .example('$0 -i myaudio.mp3 -t parse.txt -o /home/Music -m title=Test')
+  .alias('a', 'audioonly')
+  .alias('o', 'output')
+  .alias('t', 'template')
+  .alias('i', 'input')
+  .alias('m', 'metadata')
+  .alias('c', 'concurrency')
+  .alias('f', 'format')
+  .describe('o', 'Output path')
+  .describe('t', 'Template text file')
+  .describe('i', 'Input file or YouTube URL')
+  .describe('m', 'Output file metadata with "key=value" format')
+  .describe('c', 'Max concurrent tasks')
+  .describe('f', 'Output format (mp3, m4a, flac, etc)')
+  .describe('a', 'Force download only audio files when using a url as input')
+  .default('f', 'mp3')
+  .default('t', 'templ.txt')
+  .default('i', 'input.mp3')
+  .default('a', false)
+  .default('c', 3)
+  .array('m').default('m', [])
+  .help('h').alias('h', 'help')
+  .argv
+
+new Command(argv).run()
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-'use strict';
+'use strict'
 
-module.exports = require('./lib/MediaSplit.js');
+module.exports = require('./lib/MediaSplit.js')
